6699194e9a9f45bc68d053cceb173538a936a9ac
Juan Gabriel Griggio <jgriggio@fu.do>
2023-05-27 18:00:10 -0300

    feat: updates

--- getAll-index.mjs
+++ getAll-index.mjs
@@ -3,10 +3,25 @@ import {
 	DynamoDBDocumentClient,
 	ScanCommand
 } from "@aws-sdk/lib-dynamodb";
-import { HTTP_STATUS, buildReponse, tableName } from "./shared.mjs";
 
 const client = new DynamoDBClient({});
 const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = 'movies';
+
+const buildReponse = (body, statusCode) => {
+    return {
+        statusCode: statusCode ?? HTTP_STATUS.OK,
+        body: JSON.stringify(body ?? {}),
+        headers: { "Content-Type": "application/json" }
+    }
+}
+
+const HTTP_STATUS = {
+    OK: 200,
+    NO_CONTENT: 204,
+    SERVER_ERROR: 500
+}
 
 export const handler = async (event, context) => {
 	try {
@@ -15,8 +30,11 @@ export const handler = async (event, context) => {
 				TableName: tableName,
 			})
 		);
-		return buildReponse(response.Items, HTTP_STATUS.OK);
+
+		return response.Items ? buildReponse(response.Items, HTTP_STATUS.OK) : buildReponse([], HTTP_STATUS.NO_CONTENT);
+
 	} catch (err) {
-		return buildReponse(err.message ?? 'Unknown database error', HTTP_STATUS.SERVER_ERROR);
+		console.log(`ERROR: ${JSON.stringify(err)}`);
+		return buildReponse('Internal server error', HTTP_STATUS.SERVER_ERROR);
 	}
 };
--- getByName-index.mjs
+++ getByName-index.mjs
@@ -3,15 +3,38 @@ import {
 	DynamoDBDocumentClient,
 	GetCommand
 } from "@aws-sdk/lib-dynamodb";
-import { HTTP_STATUS, buildReponse, isValidString, tableName } from "./shared.mjs";
 
 const client = new DynamoDBClient({});
 const dynamo = DynamoDBDocumentClient.from(client);
 
+const tableName = 'movies';
+
+const buildReponse = (body, statusCode) => {
+    return {
+        statusCode: statusCode ?? HTTP_STATUS.OK,
+        body: JSON.stringify(body ?? {}),
+        headers: { "Content-Type": "application/json" }
+    }
+}
+
+const HTTP_STATUS = {
+    OK: 200,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    SERVER_ERROR: 500
+}
+
+const isValidString = (maybeStr) => {
+    return !(!maybeStr || typeof maybeStr !== 'string' || maybeStr.trim().length <= 0)
+}
+
 export const handler = async (event, context) => {
+	console.log(`PATH PARAMS: ${JSON.stringify(event.pathParameters)}`);
+	console.log(`NAME PARAM: ${event.pathParameters.name}`);
+	console.log(`NAME PARAM TYPE: ${typeof event.pathParameters.name}`);
 	// Parse input
 	if (!isValidString(event.pathParameters?.name))
-		return buildReponse('Invalid path parameter: name', HTTP_STATUS.BAD_REQUEST);
+		return buildReponse('Invalid or missing path parameter: name', HTTP_STATUS.BAD_REQUEST);
 
 	try {
 		const response = await dynamo.send(
@@ -22,9 +45,11 @@ export const handler = async (event, context) => {
 				},
 			})
 		);
-		return response.Item ? buildReponse(response.Item, HTTP_STATUS.OK) : buildReponse(`Movie with name: ${event.pathParameters.name} not found`, HTTP_STATUS.NOT_FOUND);
+		console.log(`RESPONSE: ${JSON.stringify(response)}`);
+		return response.Item ? buildReponse([response.Item], HTTP_STATUS.OK) : buildReponse([], HTTP_STATUS.NO_CONTENT);
 	} catch (err) {
-		return buildReponse(err.message ?? 'Unknown database error', HTTP_STATUS.SERVER_ERROR);
+		console.log(`ERROR: ${JSON.stringify(err)}`);
+		return buildReponse('Internal server error', HTTP_STATUS.SERVER_ERROR);
 	}
 };
 
