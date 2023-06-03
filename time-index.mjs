da46fe8d18e3e57eddeea8e4499b3dcbce94b957
King <crojas@itba.edu.ar>
2023-06-03 17:39:57 -0300

    Separating first response from the rest

--- time-index.mjs
+++ time-index.mjs
@@ -4,17 +4,22 @@ export const handler = async (event, context) => {
 	// Save to database
 	try {
 		const responses = [];
-		for (let i = 0; i < 5; i++) {
+		let firstResponse = 0;
+		for (let i = 0; i < 11; i++) {
 			const now = Date.now();
 			await axios.get('https://s886log05f.execute-api.us-east-2.amazonaws.com/dev/movies/peponio%20ultra%20pepe');
 			const end = Date.now();
-			responses.push(end - now);	
+			if (i == 0)
+			    firstResponse = end - now;
+			else
+			    responses.push(end - now);	
 		}
 		const sum = responses.reduce((a, b) => a + b, 0);
 		const average = (sum / responses.length) || 0;
 		const response = {
 			average: average,
-			individual: responses
+			first: firstResponse,
+			subsequent: responses
 		}
 		return {
 			statusCode: 200,
